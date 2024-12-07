6e0056ac3f76705dbef0f562cb9f08cec25e6793
tomasci <zivgta@gmail.com>
2024-12-07 21:56:01 +0300

    - added wait group and goroutines with workers; sending packets to channels and workers; function for packets processing; quit signals for goroutines. - updated address configuration for udp: separated ip and port, added creating string from ip and port, added isHost dependency to switch between localhost and all interfaces. - other changes

--- main.go
+++ main.go
@@ -2,20 +2,34 @@ package main
 
 import (
 	"errors"
+	"fmt"
 	"log"
 	"net"
 	"os"
 	"os/signal"
 	"runtime"
+	"sync"
 	"syscall"
 )
 
 const (
-	address = "0.0.0.0:25505"
+	localAddress = "127.0.0.1"
+	hostAddress  = "0.0.0.0"
+	port         = 25505
 )
 
 func main() {
 	log.Println("Server is in startup")
+
+	// todo: add support for -host option, to allow listening all other interfaces
+	isHost := false
+	var address string
+
+	if isHost {
+		address = fmt.Sprintf("%s:%d", hostAddress, port)
+	} else {
+		address = fmt.Sprintf("%s:%d", localAddress, port)
+	}
 
 	// get logical CPU core count (threads count)
 	threadsCount := runtime.NumCPU()
@@ -49,10 +63,21 @@ func main() {
 
 	log.Println("Listening on:", address)
 
-	// create channel for os Signal values, can store only one signal
-	sigChan := make(chan os.Signal, 1)
-	// listen for SIGINT & SIGTERM and direct signals to sigChan
-	signal.Notify(sigChan, syscall.SIGINT, syscall.SIGTERM)
+	// waitGroup to sync goroutines
+	var wg sync.WaitGroup
+	quit := make(chan struct{})
+	// channel for packets; main loop send packets to channel to be processed by worker goroutines
+	packetChan := make(chan []byte, 100)
+
+	// worker goroutines setup
+	// starts a number of goroutines equal to threadsCount; each goroutine is a worker that processes packets
+	// these workers started once, when server started, and alive until server stopped
+	// no more workers created during execution
+	for i := 0; i < threadsCount; i++ {
+		wg.Add(1)
+		// next: read worker func
+		go worker(i, packetChan, &wg, quit)
+	}
 
 	// main loop
 	go func() {
@@ -63,28 +88,61 @@ func main() {
 			buffer := make([]byte, 1024)
 
 			// reading data from UDP connection into a buffer
-			n, _, err := conn.ReadFromUDP(buffer) // n - number of bytes in buffer
+			n, clientAddress, err := conn.ReadFromUDP(buffer) // n - number of bytes in buffer
 			if err != nil {
 				if errors.Is(err, net.ErrClosed) {
 					log.Println("Server stopped: Connection closed")
 					break
 				}
 
-				log.Println("failed to read from UDP:", err)
-				continue
+				select {
+				case <-quit:
+					return // quit signaled
+				default:
+					log.Println("failed to read from UDP:", err)
+				}
 			}
 
-			// processing data (creating slice of data size)
-			packet := make([]byte, n)
-			// copy buffer (up to number of bytes) into packet
-			copy(packet, buffer[:n])
-
-			// todo: remove, debug purposes only
-			log.Println("Incoming packet:", string(packet))
+			select {
+			case packetChan <- buffer[:n]: // send packet to workers
+				// each received packet is sent into packetChan
+				// where they become available for processing by goroutines (and each goroutine is a separate worker?)
+				// todo: remove, debug purposes only
+				log.Printf("Incoming packet from %v (%d bytes): %v\n", clientAddress, n, string(buffer[:n]))
+			case <-quit:
+				return // break loop if quit signaled
+			}
 		}
 	}()
 
-	// wait for signal
+	// create channel for os Signal values, can store only one signal
+	sigChan := make(chan os.Signal, 1)
+	// listen for SIGINT & SIGTERM and direct signals to sigChan
+	signal.Notify(sigChan, syscall.SIGINT, syscall.SIGTERM)
+	// wait for system signal (interruption|termination signal)
 	<-sigChan
+
+	close(quit) // signal all goroutines to exit
+	close(packetChan)
+	wg.Wait() // wait for all workers to finish
 	log.Println("Shutting down")
+}
+
+func worker(id int, packetChan chan []byte, wg *sync.WaitGroup, quit <-chan struct{}) {
+	defer wg.Done() // signal to waitGroup that worker is done when function exits
+
+	// continuously receive packets from packetChan and process them
+	for {
+		select {
+		case packet := <-packetChan:
+			processPacket(id, packet)
+		case <-quit:
+			log.Printf("Worker %d stopped\n", id)
+			return
+		}
+	}
+}
+
+func processPacket(id int, packet []byte) {
+	log.Printf("Worker %d processing packet: %v\n", id, string(packet))
 }
