300d36331cbb52f0324106f4a1028a6d7a1361db
tomasci <zivgta@gmail.com>
2025-01-08 01:40:13 +0300

    - added comments for mvalidator

--- moony/core/mvalidator/mvalidator.go
+++ moony/core/mvalidator/mvalidator.go
@@ -9,10 +9,13 @@ import (
 )
 
 var (
-	once     sync.Once
+	// to ensure instance created only once
+	once sync.Once
+	// validator instance
 	validate *validator.Validate
 )
 
+// InitializeValidator – create validator instance
 func InitializeValidator() {
 	once.Do(func() {
 		log.Println("Initializing validator")
@@ -20,7 +23,9 @@ func InitializeValidator() {
 	})
 }
 
+// GetValidator – can be used to get validator globally
 func GetValidator() *validator.Validate {
+	// check if instance exists, create if not
 	if validate == nil {
 		InitializeValidator()
 	}
@@ -29,23 +34,32 @@ func GetValidator() *validator.Validate {
 }
 
 func Validate[InputType interface{}](input InputType) (*InputType, []string, error) {
+	// get validator
 	validate := GetValidator()
+	// validate with go-playground validator
 	err := validate.Struct(input)
 
+	// array for fields with errors
 	errorFields := make([]string, 0)
 
 	if err != nil {
+		// go through all errors
 		for _, err := range err.(validator.ValidationErrors) {
+			// search field in struct
 			structField, _ := reflect.TypeOf(input).FieldByName(err.StructField())
+			// get it's json name
 			jsonTag := structField.Tag.Get("json")
+			// collect field names
 			errorFields = append(errorFields, jsonTag)
 		}
 
-		log.Println(errorFields)
+		//log.Println(errorFields)
 		log.Println("validation_failed", err.Error())
 
+		// return error
 		return nil, errorFields, errors.New("validation_failed")
 	}
 
+	// return result with input
 	return &input, errorFields, nil
 }
